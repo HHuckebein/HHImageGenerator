a66b7e0beb3a8cbefca325599aa9baa1c343fc1e
RABE_IT Services <info@berndrabe.de>
2015-09-06 09:11:42 +0200

    Initial Commit

---
+++ HHImageGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HHImageGenerator
+//
+//  Created by Bernd Rabe on 06.09.15.
+//  Copyright (c) 2015 RABE_IT Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
